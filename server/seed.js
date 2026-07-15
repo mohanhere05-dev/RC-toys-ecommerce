032db602021c510383703e660b3304fc2cec0369
mohanhere05-dev <mohanhere05@gmail.com>
2026-07-15 20:15:07 +0530

    Convert CommonJS to ES Modules

--- server/seed.js
+++ server/seed.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const connectDB = require("./config/db");
-const Product = require("./models/Product");
-const products = require("./data/products");
+import connectDB from "./config/db.js";
+import Product from "./models/Product.js";
+import products from "./data/products.js";
 
 dotenv.config();
 
