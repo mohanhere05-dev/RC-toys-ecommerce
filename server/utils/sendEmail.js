264d45f1793bff5ae403d80c00145f65b380c36a
mohanhere05-dev <mohanhere05@gmail.com>
2026-07-17 14:49:17 +0530

    Replace Gmail SMTP with Resend

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,20 +4,23 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
+
 const sendEmail = async (to, subject, text) => {
-  await transporter.sendMail({
-    from: `"TurboToys" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-  });
+    await transporter.sendMail({
+        from: `"TurboToys" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        text,
+    });
 };
 
 export default sendEmail;
