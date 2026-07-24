b7fa20d9f9f7a906efa432569eac116890bbb302
mohanhere05-dev <mohanhere05@gmail.com>
2026-07-24 21:07:47 +0530

    Error handled

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -22,12 +22,4 @@ const sendEmail = async (to, subject, text) => {
     }
 };
 
-import dotenv from "dotenv";
-dotenv.config();
-
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-
-    export default sendEmail;
+export default sendEmail;
